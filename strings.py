d5e844bca9b0fdf428fd01f5c520512f55e687b3
bhanuprakashreddy <bhanukamjula@gmail.com>
2023-07-31 19:02:47 +0530

    adding

---
+++ strings.py
@@ -0,0 +1,35 @@
+# multiline strings
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# looping through strings
+for x in "Reddy":
+  print(x)
+
+# Strings in array
+b= "Bhanu Prakash"
+print(b[1])
+
+# string length
+c = "bhanu prakash"
+print(len(c))
+
+# check string
+txt = "kamjula bhanu prakash reddy"
+print("reddy" in txt)
+
+# string slicing
+d = "bhanu prakash"
+print(d[2:5])
+
+# slice from start
+e= "bhanu prakash"
+print(e[:5])
+
+# slice to the end
+f = "bhanunprakash"
+print(f[2:])
